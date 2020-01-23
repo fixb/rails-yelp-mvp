7de46d0bd737a50015862f7ec5a11170af33fe97
Francois Baud <fxbaud@gmail.com>
2020-01-23 19:29:07 +0000

    style yet to be done...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/show'
   resources :restaurants do
     resources :reviews, only: [:new, :create]
   end
+    resources :reviews, only: [:show, :edit, :update, :destroy]
 end
